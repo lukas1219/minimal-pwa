49e561a34e526c53953b85fef2ee6369448c4c41
Lukas Sartori <lukas1218@gmail.com>
2021-04-12 19:54:17 +0200

    initial commit

---
+++ sw.js
@@ -0,0 +1,28 @@
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open('FILL_IN_CACHE_NAME_v1')
+            .then(cache => cache.addAll([
+                'index.html',
+                'main.js',
+                'style.css',
+                'icon16.png',
+                'icon192.png',
+                'icon196.png',
+                'icon512.png',
+                'manifest.webmanifest'
+            ]))
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches
+        .open("FILL_IN_CACHE_NAME_v1")
+        .then(function (cache) {
+          return cache.match(event.request);
+        })
+        .then(function (response) {
+          return response || fetch(event.request);
+        })
+    );
+});
